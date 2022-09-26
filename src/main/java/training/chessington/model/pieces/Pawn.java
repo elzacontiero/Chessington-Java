10d6fe264b1298b87662f5a042ab4b2cb0b6f03d
Elza Contiero <contieroelza@gmail.com>
2022-09-26 15:30:28 +0100

    Refactor black pawn movements to its own method.

--- src/main/java/training/chessington/model/pieces/Pawn.java
+++ src/main/java/training/chessington/model/pieces/Pawn.java
@@ -12,7 +12,47 @@ public class Pawn extends AbstractPiece {
     public Pawn(PlayerColour colour) {
         super(Piece.PieceType.PAWN, colour);
     }
+    private List<Move> getAllowedMovesForBlack(Coordinates from, Board board) {
 
+        List<Move> result = new ArrayList<>();
+        // Black moves down +1
+        Coordinates to = from.plus(1,0);
+        if (to.getRow()>=0 && to.getRow()<=7) {
+            Piece pieceInFront = board.get(to);  // get piece in to
+            if (pieceInFront == null) { // there is no piece in front
+                Move ft2 = new Move(from, to);
+                result.add(ft2);
+            }
+        }
+        //
+        if(from.getRow()==1) {
+            to = from.plus(2, 0);// first move of pawn can move 2 squares
+            Move ft3 = new Move(from, to);
+            result.add(ft3);
+        }
+
+        // blackPawnsCanCaptureDiagonally
+        to = from.plus(1,1);
+        if (to.getRow() <= 7 && to.getCol()<=7) {
+            Piece enemy = board.get(to);
+            if (enemy != null && enemy.getColour() == PlayerColour.WHITE) {
+                Move move = new Move(from, to);
+                result.add(move);
+            }
+        }
+
+        to = from.plus(1, -1);
+        //to.column is -1, which is not allowed. need to put a check before next line
+        // We have attempted to add +1 row and -1 column. So, row could go over 7 and column less than 0. The check below ensures this doesn't happen.
+        if (to.getRow()<=7 && to.getCol()>=0) {
+            Piece enemy = board.get(to);
+            if (enemy != null && enemy.getColour() == PlayerColour.WHITE) {
+                Move move = new Move(from, to);
+                result.add(move);
+            }
+        }
+        return result;
+    }
     @Override
     public List<Move> getAllowedMoves(Coordinates from, Board board) {
         List<Move> result = new ArrayList<>();
@@ -27,22 +67,6 @@ public class Pawn extends AbstractPiece {
             }
         }
 
-        // Black moves down +1
-        to = from.plus(1,0);
-        if (to.getRow()>=0 && to.getRow()<=7) {
-            pieceInFront = board.get(to);
-            if (colour == PlayerColour.BLACK && pieceInFront == null) {
-                Move ft2 = new Move(from, to);
-                result.add(ft2);
-            }
-        }
-
-        if(colour == PlayerColour.BLACK && from.getRow()==1) {
-            to = from.plus(2, 0);
-            Move ft3 = new Move(from, to);
-            result.add(ft3);
-        }
-
         if(colour==PlayerColour.WHITE && from.getRow()==6) {
             to = from.plus(-2, 0);
             Move ft4 = new Move(from, to);
@@ -51,35 +75,29 @@ public class Pawn extends AbstractPiece {
 
         // whitePawnsCanCaptureDiagonally
         if (colour==PlayerColour.WHITE) {
-            to = from.plus(-1,1);
-            Piece enemy = board.get(to);
-            if (enemy != null && enemy.getColour() == PlayerColour.BLACK) {
-                Move move = new Move(from, to);
-                result.add(move);
+            to = from.plus(-1, 1);
+            if(to.getRow()>=0 && to.getCol()<=7) {
+                Piece enemy = board.get(to);
+                if (enemy != null && enemy.getColour() == PlayerColour.BLACK) {
+                    Move move = new Move(from, to);
+                    result.add(move);
+                }
             }
             to = from.plus(-1,-1);
-            enemy = board.get(to);
-            if (enemy != null && enemy.getColour() == PlayerColour.BLACK) {
-                Move move = new Move(from, to);
-                result.add(move);
-            }
-        }
-        // blackPawnsCanCaptureDiagonally
-        if (colour==PlayerColour.BLACK) {
-            to = from.plus(1,1);
-            Piece enemy = board.get(to);
-            if (enemy != null && enemy.getColour() == PlayerColour.WHITE) {
-                Move move = new Move(from, to);
-                result.add(move);
-            }
-            to = from.plus(1,-1);
-            enemy = board.get(to);
-            if (enemy != null && enemy.getColour() == PlayerColour.WHITE) {
-                Move move = new Move(from, to);
-                result.add(move);
+            if(to.getRow()>=0 && to.getCol()>=0) {
+                Piece enemy = board.get(to);
+                if (enemy != null && enemy.getColour() == PlayerColour.BLACK) {
+                    Move move = new Move(from, to);
+                    result.add(move);
+                }
             }
         }
 
+        if (colour==PlayerColour.BLACK) {
+            List<Move> blackMovements = getAllowedMovesForBlack(from, board);
+            result.addAll(blackMovements);
+        }
         return result;
-    }
+      }
 }
+
